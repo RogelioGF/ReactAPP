858b94eb03f8be4179e67a60076b5ffe530da094
60020175 <60020175+RogelioGF@users.noreply.github.com>
2021-04-13 15:24:21 +0200

    lista de tareas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
 import './App.css';
+import Title from "./components/title";
+import Layout from "./components/layout";
+import TaskList from "./components/task-list";
+import Input from "./components/input";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            newTask: "",
+            tasks: [],
+        }
+    }
+
+    handleTaskChange = (event) => {
+        this.setState({
+            newTask: event.target.value,
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        let newTask = this.state.tasks;
+        newTask.push(this.state.newTask);
+        this.setState({
+            newTask: "",
+            tasks: newTask
+        })
+    }
+
+    handleDelete = (id) => {
+        let newTasks = this.state.tasks;
+        newTasks.splice(id, 1)
+        this.setState({
+            tasks: newTasks,
+        })
+    }
+
+    render() {
+        return (
+            <Layout>
+                <Title/>
+                <Input
+                    handleSubmit={this.handleSubmit}
+                    handleTaskChange={this.handleTaskChange}
+                    value={this.state.newTask}
+                />
+                <TaskList
+                    tasks={this.state.tasks}
+                    handleDelete={this.handleDelete}
+                />
+            </Layout>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/title.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Title() {
+    return (
+        <h1 className="title">
+            Listado de tareas{" "}
+            <span aria-label="emoji" role="img">
+        🔥
+      </span>
+        </h1>
+    );
+}
+
+export default Title;
